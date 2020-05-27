fe348d9be088e4ec149b87f00ff33e51a6807052
Noah Pflaum <pflauming@gmail.com>
2020-05-27 12:29:27 -0600

    Adding components

--- face_detector/face_detector.py
+++ face_detector/face_detector.py
@@ -1,28 +1,53 @@
 import numpy as np
 import cv2 as cv
-import sys
-print(sys.path)
+import paho.mqtt.client as mqtt
 
-# 1 should correspond to /dev/video1 , your USB camera. The 0 is reserved for the TX2 onboard camera
-cap = cv.VideoCapture(0)
+LOCAL_MQTT_HOST="broker"
+LOCAL_MQTT_PORT=1883
+LOCAL_MQTT_TOPIC="facial_images"
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
 
-    # We don't use the color information, so might as well save space
-    face_cascade = cv.CascadeClassifier('/Users/npflaum/anaconda3/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml')
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+class FaceDetector:
+    def __init__(self, mqtt_client):
+        self.mqtt_client = mqtt_client
+    
+    def process(self):
+        cap = cv.VideoCapture(1)
 
-    for (x,y,w,h) in faces:
-        face = gray[y:y+h, x:x+w]
-        cv.imshow('frame', face)
-        # rc, png = cv.imencode('.png', face)
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
 
-    # cv.imshow('frame', gray)
+            # We don't use the color information, so might as well save space
+            face_cascade = cv.CascadeClassifier('/usr/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
-    # print(gray)
-    # face detection and other logic goes here
+            for (x,y,w,h) in faces:
+                face = gray[y:y+h, x:x+w]
+                cv.imshow('frame', face)
+                rc, png = cv.imencode('.png', face)
+                message = png.tobytes()
+                self.mqtt_client.publish(LOCAL_MQTT_TOPIC, message)
+
+            # cv.imshow('frame', gray)
+
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected facial image publisher to local broker with rc: " + str(rc))
+
+
+def on_publish(client, userdata, mid):
+    print("A face has just been published")
+
+
+
+local_mqtt_client = mqtt.Client()
+local_mqtt_client.on_connect = on_connect
+local_mqtt_client.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
+
+face_detector = FaceDetector(local_mqtt_client)
+face_detector.process()
